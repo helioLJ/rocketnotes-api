488ab8f931f217a9622caf94b71565a9ff1363a6
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-04-03 10:38:27 -0300

    utilização do describe() do Jest

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,18 +1,24 @@
 const UserCreateService = require('./UserCreateService')
 const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory')
 
-it("user should be create", async () => {
-  const user = {
-    name: "User Test",
-    email: "user@test.com",
-    password: "123"
-  };
+describe("UserCreateService", () => {
+  it("user should be create", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123"
+    };
 
-  const userRepositoryInMemory = new UserRepositoryInMemory()
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user);
+    const userRepositoryInMemory = new UserRepositoryInMemory()
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+    const userCreated = await userCreateService.execute(user);
 
-  console.log(userCreated);
+    console.log(userCreated);
 
-  expect(userCreated).toHaveProperty("id");
+    expect(userCreated).toHaveProperty("id");
+  });
+
+  it("", () => {
+
+  });
 });
